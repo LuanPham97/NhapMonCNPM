aed39482ba96467fa6db771465b96c832d4d8d51
Nguyen-Trung-Nhan <nguyentrungnhan23897@gmail.com>
2019-05-31 17:12:04 +0700

    pTuNhan.php

--- Source code/Prisonproject/admin/Pages/pTuNhan.php
+++ Source code/Prisonproject/admin/Pages/pTuNhan.php
@@ -1,5 +1,5 @@
 <h1 align="Center">Quản Lý Tù Nhân</h1>
 <h4>Thêm Thông Tin Tù Nhân: <a href="index.php?a=8"><button>Thêm</button></a></h4>
 <h4>Xóa Thông Tin Tù Nhân: <a href="index.php?a=9"><button>Xóa</button></a></h4>
-<h4>Sửa Thông Tin Tù Nhân: <a href="index.php?a=10"><button>Sửa</button></a></h4>
-<h4>Liệt Kê Tù Nhân: <a href="index.php?a=11"><button>Liệt Kê</button></a></h4>
+<h4>Cập Nhật Thông Tin Tù Nhân: <a href="index.php?a=10"><button>Cập Nhật</button></a></h4>
+<h4>Xem Danh Sách Tù Nhân: <a href="index.php?a=11"><button>Xem</button></a></h4>
