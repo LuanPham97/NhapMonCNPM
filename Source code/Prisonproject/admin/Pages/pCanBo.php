2ff20c4aeb3827b06214202fdec8e0dcc95d53a4
Nguyen-Trung-Nhan <nguyentrungnhan23897@gmail.com>
2019-05-31 17:05:12 +0700

    update CanBo

--- Source code/Prisonproject/admin/Pages/pCanBo.php
+++ Source code/Prisonproject/admin/Pages/pCanBo.php
@@ -2,4 +2,4 @@
 <h4>Thêm Thông Tin Cán Bộ: <a href="index.php?a=13"><button>Thêm</button></a></h4>
 <h4>Xóa Thông Tin Cán Bộ: <a href="index.php?a=15"><button>Xóa</button></a></h4>
 <h4>Sửa Thông Tin Cán Bộ: <a href="index.php?a=14"><button>Sửa</button></a></h4>
-<h4>Liệt Kê Thông Tin Cán Bộ: <a href="index.php?a=16"><button>Liệt Kê</button></a></h4>
+<h4>Xem Danh Sách Thông Tin Cán Bộ: <a href="index.php?a=16"><button>Xem</button></a></h4>
